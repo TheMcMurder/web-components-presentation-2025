2f0475bb4f6b9aa40070a22a0751a5037843519b
the_mcmurder <3059715+TheMcMurder@users.noreply.github.com>
2025-02-06 18:00:22 -0700

    working basic custom-element only example

--- apps/custom-element/src/main.ts
+++ apps/custom-element/src/main.ts
@@ -1,1 +1,64 @@
-console.log("Here we go");
+// Input component
+class NameInput extends HTMLElement {
+  inputElement: HTMLInputElement | null;
+  constructor() {
+    super();
+    this.innerHTML = `
+      <input type="text" placeholder="Enter a name">
+    `;
+    this.inputElement = this.querySelector('input');
+  }
+
+  get value() {
+    return this.inputElement ? this.inputElement.value : undefined;
+  }
+
+  clear() {
+    if (this.inputElement) {
+      this.inputElement.value = '';
+    }
+  }
+}
+
+// Button component
+class UpdateButton extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = `
+      <button>Update Name</button>
+    `;
+    this.addEventListener('click', () => {
+      this.dispatchEvent(new CustomEvent('update-name', { bubbles: true }));
+    });
+  }
+}
+
+// Display component
+class NameDisplay extends HTMLElement {
+  constructor() {
+    super();
+    this.render();
+  }
+
+  static get observedAttributes() {
+    return ['name'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'name') {
+      this.render();
+    }
+  }
+
+  render() {
+    const name = this.getAttribute('name') || 'World';
+    this.innerHTML = `
+      <div>Hello, ${name}!</div>
+    `;
+  }
+}
+
+// Register custom elements
+customElements.define('name-input', NameInput);
+customElements.define('update-button', UpdateButton);
+customElements.define('name-display', NameDisplay);
